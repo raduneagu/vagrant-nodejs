dddb5ad35f66c06e5cbcdee7643f3f70c29e4b98
Radu Neagu <radu@dotweb.ro>
2014-08-10 22:47:23 +0300

    ! Removed mongo and redis

--- vagrant/puppet/manifests/main.pp
+++ vagrant/puppet/manifests/main.pp
@@ -52,34 +52,6 @@ class node-js {
   }
 }
 
-class mongodb {
-  exec { "mongodbKeys":
-    command => "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10",
-    path => ["/bin", "/usr/bin"],
-    notify => Exec["aptGetUpdate"],
-    unless => "apt-key list | grep mongodb"
-  }
-
-  file { "mongodb.list":
-    path => "/etc/apt/sources.list.d/mongodb.list",
-    ensure => file,
-    content => "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen",
-    notify => Exec["mongodbKeys"]
-  }
-
-  package { "mongodb-org":
-    ensure => present,
-    require => [Exec["aptGetUpdate"],File["mongodb.list"]]
-  }
-}
-
-
-class redis-cl {
-  class { 'redis': }
-}
-
 include apt_update
 include othertools
 include node-js
-include mongodb
-include redis-cl
